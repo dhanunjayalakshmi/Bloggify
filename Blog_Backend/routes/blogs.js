a378b1c00f8504402867c5534f53806f5f47db7e
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2025-03-26 20:38:02 +0530

    Implemented the functionality to fetch all blogs with pagination, filtering of sort, search and tags

--- Blog_Backend/routes/blogs.js
+++ Blog_Backend/routes/blogs.js
@@ -7,6 +7,60 @@ const { verifyToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 const upload = multer();
+
+// Get all blogs
+router.get("/", async (req, res) => {
+  try {
+    let { page, limit, search, sort, tags } = req?.query;
+
+    page = page ? parseInt(page) : 1;
+    limit = limit ? parseInt(limit) : 10;
+    const offset = (page - 1) * limit;
+
+    let query = supabase
+      .from("blogs")
+      .select("*", { count: "exact" })
+      .eq("is_published", true);
+
+    if (search) {
+      query = query.or(
+        `title.ilike.%${search.toLowerCase()}%, content.ilike.%${search.toLowerCase()}%`
+      );
+    }
+
+    if (tags) {
+      const tagsArray = tags.includes(",") ? tags.split(",") : [tags];
+      const tagsLowerCase = tagsArray.map((tag) => tag.toLowerCase());
+      query = query.contains("tags", tagsLowerCase);
+    }
+
+    if (sort === "newest") {
+      query = query.order("published_at", { ascending: false });
+    } else if (sort === "oldest") {
+      query = query.order("published_at", { ascending: true });
+    } else if (sort === "read_time") {
+      query = query.order("read_time", { ascending: true });
+    }
+
+    query = query.range(offset, offset + limit - 1);
+
+    const { data, error, count } = await query;
+
+    if (error) throw error;
+
+    res.status(200).json({
+      blogs: data,
+      total: count,
+      page,
+      limit,
+      totalPages: Math.ceil(count / limit),
+    });
+  } catch (error) {
+    res.status(500).json({ error: error?.message });
+  }
+});
+
+// Create new blog
 router.post("/", verifyToken, upload.single("image"), async (req, res) => {
   try {
     const user = req?.user;
