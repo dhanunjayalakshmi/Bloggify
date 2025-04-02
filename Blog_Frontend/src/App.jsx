642aca0993109dcd4abfe3f9386f0377d5900eb0
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2025-04-02 17:40:36 +0530

    Initial setup is done

---
+++ Blog_Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="w-20 mt-10 mx-auto">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
