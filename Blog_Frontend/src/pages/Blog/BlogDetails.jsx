b8e25e2ba617f22fd5b7bb2140196345aaf9c4b6
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2025-08-24 04:14:56 +0530

    Clearing console logs

--- Blog_Frontend/src/pages/Blog/BlogDetails.jsx
+++ Blog_Frontend/src/pages/Blog/BlogDetails.jsx
@@ -47,8 +47,6 @@ const BlogDetails = () => {
     cover_image: coverImageUrl,
   } = blog;
 
-  console.log("Rendered blog content:", content);
-
   return (
     <div className="max-w-4xl mx-auto p-8 my-8 rounded-lg dark:bg-gray-800 bg-white text-gray-900 dark:text-white">
       {/* Title */}
@@ -105,7 +103,6 @@ const BlogDetails = () => {
         </Button>
       </div>
 
-      {/* Cover Image */}
       {coverImageUrl && (
         <div className="mt-6 mb-8">
           <img
@@ -116,12 +113,10 @@ const BlogDetails = () => {
         </div>
       )}
 
-      {/* Blog Content */}
       <div className="prose prose-lg dark:prose-invert tiptap max-w-none mt-6 space-y-2">
         <BlogContentRenderer content={content} />
       </div>
 
-      {/* Tags */}
       {tags?.length > 0 && (
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag, idx) => (
