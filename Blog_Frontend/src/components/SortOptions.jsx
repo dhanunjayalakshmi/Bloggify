b92c46c450271a9b16d45fc65a656ee982d0d5cb
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2026-01-03 11:35:08 +0530

    State uplifted from sortoptions component

--- Blog_Frontend/src/components/SortOptions.jsx
+++ Blog_Frontend/src/components/SortOptions.jsx
@@ -1,19 +1,16 @@
-import React, { useState } from "react";
 import { Button } from "./ui/button";
 
 const sortOptions = ["popular", "New", "Trending"];
 
-const SortOptions = () => {
-  const [selected, setSelected] = useState("popular");
-
+const SortOptions = ({ value, onChange }) => {
   return (
     <div className="flex gap-4 mb-4 py-4">
       {sortOptions?.map((option) => (
         <Button
           key={option}
-          onClick={() => setSelected(option)}
+          onClick={() => onChange(option)}
           className={`tarnsition-all duration-200 ${
-            selected === option
+            value === option
               ? "text-white"
               : "bg-muted text-muted-foreground hover:bg-accent hover:text-black dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-600"
           }`}
--- Blog_Frontend/src/pages/Home/Home.jsx
+++ Blog_Frontend/src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import { useEffect, useRef, useState } from "react";
 const Home = () => {
   const sidebarEndRef = useRef(null);
   const [isSidebarBottomVisible, setIsSidebarBottomVisible] = useState(false);
+  const [sort, setSort] = useState("popular");
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -34,8 +35,8 @@ const Home = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-4 pt-4">
       <div className="lg:col-span-2 space-y-6">
-        <SortOptions />
-        <BlogList />
+        <SortOptions value={sort} onChange={setSort} />
+        <BlogList sort={sort} />
       </div>
 
       <aside
