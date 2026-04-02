654ee8e4e14098ee211641939fb26c36b014ef85
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2026-04-02 16:21:01 +0530

    Removed duplicate Auth Initialization

--- Blog_Frontend/src/routes/AppRoutes.jsx
+++ Blog_Frontend/src/routes/AppRoutes.jsx
@@ -19,14 +19,11 @@ import AboutPage from "@/pages/LandingPage/AboutPage";
 import ContactPage from "@/pages/LandingPage/ContactPage";
 import TermsPage from "@/pages/LandingPage/TermsPage";
 import PublicLayout from "@/layouts/PublicLayout";
-import useAuthInit from "@/hooks/useAuthInit";
 import Explore from "@/pages/Explore";
 import FollowListPage from "@/pages/User/FollowListPage";
 import ExplorePeople from "@/pages/ExplorePeople";
 
 const AppRoutes = () => {
-  useAuthInit();
-
   return (
     <Routes>
       <Route element={<PublicLayout />}>
